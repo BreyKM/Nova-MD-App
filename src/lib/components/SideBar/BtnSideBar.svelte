22f99055ef347ccc7b87f203ca90ca249793bf7e
BredanKM <bredankm@gmail.com>
2024-02-27 15:20:42 -0600

    added non-functional sidebar buttons

---
+++ src/lib/components/SideBar/BtnSideBar.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let onClick;
+  export let padding;
+  export let margin;
+  export let bgColor;
+</script>
+
+<button style="padding: {padding}; margin: {margin}; background-color: {bgColor}" on:click={onClick}
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.75em"
+    height="1.75em"
+    viewBox="0 0 24 24"
+    {...$$props}
+    ><path fill=""
+    stroke=""
+    stroke-linecap=""
+    stroke-linejoin=""
+    stroke-width="" d="" {...$$props} />
+  </svg></button
+>
+
+
