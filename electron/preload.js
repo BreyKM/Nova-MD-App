98ca75b65c8c097ee14fefd380d94e3f1ed45e2b
BredanKM <bredankm@gmail.com>
2024-02-27 13:15:32 -0600

    add titlebar drag and buttons functionality

--- electron/preload.js
+++ electron/preload.js
@@ -1,9 +1,15 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 const api = {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron
 }
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    minimize: () => ipcRenderer.send('minimize'),
+    maximize: () => ipcRenderer.send('maximize'),
+    close: () => ipcRenderer.send('close')
+})
 
 contextBridge.exposeInMainWorld('api', api)
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,17 @@ export default {
         "./electron/**/*.{html,svelte,js,ts,}"
     ],
     theme: {
-        colors: {
-            'black': '#2B2B2B'
-        },
         extend: {
             fontFamily: {
                 jost: ['Jost', "sans-serif"]
+            },
+            colors: {
+                'black': '#2B2B2B',
+                'black-900': '#1f1f1f',
+                'bone' : '#fafafa',
+                'hover-gray': '#474747',
+                'hover-red': '#FF4D4D',
+                'nav-gray': '#3D3D3D'
             }
         },
     },
